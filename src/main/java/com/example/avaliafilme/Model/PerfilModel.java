49dc6a55c2ee9cea0dfafa4cb2a5562aaf84e741
Sammuel Victor <samvss13@gmail.com>
2026-05-17 18:13:08 +0000

    Criação do PerfilModel.java

---
+++ src/main/java/com/example/avaliafilme/Model/PerfilModel.java
@@ -0,0 +1,28 @@
+package com.example.avaliafilme.Model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "perfis")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class PerfilModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(length = 255)
+    private String biografia;
+
+    @Column(name = "foto_url", length = 500)
+    private String fotoUrl;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private userModel user;
+}
