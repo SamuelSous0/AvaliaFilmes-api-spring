8c2b3a56103aabed51d4715607c89bc5630b1432
Sammuel Victor <samvss13@gmail.com>
2026-05-17 18:22:04 +0000

    Criação do PerfilResponseDTO.java

---
+++ src/main/java/com/example/avaliafilme/dto/PerfilResponseDTO.java
@@ -0,0 +1,17 @@
+package com.example.avaliafilme.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
+public class PerfilResponseDTO {
+
+    private long id;
+    private String biografia;
+    private String fotoUrl;
+    private String username;
+}
