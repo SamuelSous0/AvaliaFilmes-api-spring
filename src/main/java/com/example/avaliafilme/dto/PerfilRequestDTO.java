885348467c129faafad01912a254669a4665e55a
Sammuel Victor <samvss13@gmail.com>
2026-05-17 18:17:12 +0000

    Criação do PerfilResquestDTO.java

---
+++ src/main/java/com/example/avaliafilme/dto/PerfilRequestDTO.java
@@ -0,0 +1,13 @@
+package com.example.avaliafilme.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PerfilRequestDTO {
+
+    private String biografia;
+    private String fotoUrl;
+    private long userId;
+}
