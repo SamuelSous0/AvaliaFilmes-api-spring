0998d73d756d2bc148e2de525f3a8f8687cec32a
JvCruzM <cruzdemenezesjv@gmail.com>
2026-05-25 13:00:41 -0300

    Criação do ReviewController.java

---
+++ src/main/java/com/example/avaliafilme/controller/ReviewController.java
@@ -0,0 +1,43 @@
+package com.example.avaliafilme.controller;
+
+import com.example.avaliafilme.dto.ReviewRequestDTO;
+import com.example.avaliafilme.dto.ReviewResponseDTO;
+import com.example.avaliafilme.service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/reviews")
+public class ReviewController {
+
+    @Autowired
+    private ReviewService reviewService;
+
+    @PostMapping
+    public ResponseEntity<ReviewResponseDTO> criarReview(@RequestBody ReviewRequestDTO request) {
+        ReviewResponseDTO response = reviewService.criarReview(request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ReviewResponseDTO>> listarTodas() {
+        List<ReviewResponseDTO> reviews = reviewService.listarTodas();
+        return ResponseEntity.ok(reviews);
+    }
+
+    @GetMapping("/filme/{filmeId}")
+    public ResponseEntity<List<ReviewResponseDTO>> listarPorFilme(@PathVariable Long filmeId) {
+        List<ReviewResponseDTO> reviews = reviewService.listarPorFilme(filmeId);
+        return ResponseEntity.ok(reviews);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarReview(@PathVariable Long id) {
+        reviewService.deletarReview(id);
+        return ResponseEntity.noContent().build();
+    }
+}
