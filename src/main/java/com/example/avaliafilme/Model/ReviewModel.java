b0257d53f1ce34210effec00a44184f58f957769
JvCruzM <cruzdemenezesjv@gmail.com>
2026-05-25 11:38:41 -0300

    Criação do ReviewModel.java

---
+++ src/main/java/com/example/avaliafilme/Model/ReviewModel.java
@@ -0,0 +1,43 @@
+package com.example.avaliafilme.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_review")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReviewModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private double nota;
+
+    @Column(length = 1000)
+    private String comentario;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime dataCriacao;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "filme_id", nullable = false)
+    private FilmeModel filme;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "perfil_id", nullable = false)
+    private PerfilModel perfil;
+
+    @PrePersist
+    protected void onCreate() {
+        this.dataCriacao = LocalDateTime.now();
+    }
+}
