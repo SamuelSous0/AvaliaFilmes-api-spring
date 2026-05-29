8e26d5345598f963119e9b157b1237e58a6c70ec
sofia-viana <sofiavianax9@gmail.com>
2026-05-29 21:34:14 +0000

    post-perfil controller

---
+++ src/main/java/com/example/avaliafilme/controller/PostPerfilController.java
@@ -0,0 +1,37 @@
+package com.example.avaliafilme.controller;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/post-perfil")
+public class PostPerfilController {
+    @Autowired
+    private PostPerfilService service;
+
+    @GetMapping("/perfil/{perfilId}")
+    public List<PostPerfilModel> listarPorPerfil(@PathVariable Long perfilId){
+        return service.listarPorPerfil(perfilId);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<PostPerfilModel> buscarporId(@PathVariable Long id){
+        return service.buscarporId(id)
+                .map(ResponseEntity::ok).orElse(ResponseEntity.NOT_FOUND().build());
+    }
+    @PostMapping
+    public PostPerfilModel criar (@RequestBody PostPerfilModel postPerfil){
+        return service.criar(postPerfil);
+    }
+    @PutMapping ("/{id}")
+    public ResponseEntity<PostPerfilModel> atualizar (@PathVariable Long id, @RequestBody PostPerfilModel dadosAtt){
+        try{
+            return ResponseEntity.ok(service.atualizar(id, dadosAtt));    
+        } catch(RuntimeException e) {
+            return ResponseEntity.NOT_FOUND().build();
+        }
+    }
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id){
+        service.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+}
