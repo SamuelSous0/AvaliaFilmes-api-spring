457ab8db2cdefe38ca99956cf428173859527f6c
SamuelSous0 <samuel.00000859887@unicap.br>
2026-05-30 09:15:14 -0300

    Corrige erros de digitação e nomenclatura

--- src/main/java/com/example/avaliafilme/Model/PostPerfilModel.java
+++ src/main/java/com/example/avaliafilme/Model/PostPerfilModel.java
@@ -6,12 +6,11 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;;
 
-@Enity
+@Entity
 @Table(name = "post_perfil")
 @NoArgsConstructor
 @AllArgsConstructor
-@BodyBuilder
-
+@Builder
 public class PostPerfilModel{
     @Id
     @GeneratedValue
